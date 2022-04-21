935a18251268ef804900e987a69f882f6fb7996c
SamanKeighobadi <saman.keighobadi@gmail.com>
2022-04-21 17:47:09 +0430

    feat: add customer food

--- src/redux/features/customerSlice.js
+++ src/redux/features/customerSlice.js
@@ -1,18 +1,24 @@
-import {createSlice} from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit";
 
 export const customerSlice = createSlice({
-    name:"customer",
-    initialState:{
-        value:[]
+  name: "customer",
+  initialState: {
+    value: [],
+  },
+  reducers: {
+    addCustomer: (state, action) => {
+      state.value.push(action.payload);
     },
-    reducers:{
-        addCustomer:(state,action) =>{
-            state.value.push(action.payload)
+    addFoodToCustomer: (state, action) => {
+      state.value.forEach((customer) => {
+        if (customer.id === action.payload.id) {
+          customer.food.push(action.payload.food);
         }
-    }
+      });
+    },
+  },
 });
 
-export const {addCustomer} = customerSlice.actions
-
+export const { addCustomer, addFoodToCustomer } = customerSlice.actions;
 
 export default customerSlice.reducer;
