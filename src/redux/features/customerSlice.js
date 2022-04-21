3ecaa7a507adc29cc4692f3e9da98929bf5af5af
SamanKeighobadi <saman.keighobadi@gmail.com>
2022-04-21 17:32:35 +0430

    feat: create customer slice

---
+++ src/redux/features/customerSlice.js
@@ -0,0 +1,16 @@
+import {createSlice} from '@reduxjs/toolkit'
+
+export const customerSlice = createSlice({
+    name:"customer",
+    initialState:{
+        value:[]
+    },
+    reducers:{
+        addCustomer:(state,action) =>{
+            
+        }
+    }
+});
+
+
+export default customerSlice.reducer;
